7844143515bca4ff79f779a3d037e00d433842cd
Sun4n <kazisunan68@gmail.com>
2026-03-07 00:13:09 +0600

    build login js logic

--- script/login.js
+++ script/login.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const userName = document.getElementById("userName")
+const password= document.getElementById("password")
+
+
+
+document.getElementById("signIn").addEventListener("click", ()=>{
+    if (userName.value=="admin" && password.value == "admin1234") {
+        window.location.assign("home.html")
+        
+    }
+    
+    
+})
